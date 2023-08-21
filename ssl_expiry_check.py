b028c7645e6054c576f82c737e101ab7d9e2c6bd
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2023-08-21 15:47:50 +0530

    Update ssl_expiry_check.py

--- ssl_expiry_check.py
+++ ssl_expiry_check.py
@@ -1,3 +1,4 @@
+import json
 import ssl
 import socket
 import datetime
@@ -12,25 +13,19 @@ def get_ssl_expiry(domain):
             return datetime.datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
 
 def main():
-    domains = ["facebook.com", "tesla.com", "google.com"]  # Add your domains here
-    for domain in domains:
-        expiry_date = get_ssl_expiry(domain)
-        remaining_days = (expiry_date - datetime.datetime.now()).days
-        message = (
-            f"SSL Expiry Alert\n"
-            f"* Domain : {domain}\n"
-            f"* Warning : The SSL certificate for {domain} will expire in {remaining_days} days."
-        )
-        payload = {"text": message}
-        slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
-        if slack_webhook_url:
-            response = requests.post(slack_webhook_url, json=payload)
-            if response.status_code == 200:
-                print(f"Alert sent for {domain}")
-            else:
-                print(f"Failed to send alert for {domain}: {response.status_code}")
-        else:
-            print("No Slack webhook URL provided. Skipping alert.")
+    with open('domains.json') as config_file:
+        config = json.load(config_file)
+        domains = config.get('domains', [])
+        
+        for domain in domains:
+            expiry_date = get_ssl_expiry(domain)
+            remaining_days = (expiry_date - datetime.datetime.now()).days
+            message = (
+                f"SSL Expiry Alert\n"
+                f"* Domain : {domain}\n"
+                f"* Warning : The SSL certificate for {domain} will expire in {remaining_days} days."
+            )
+            print(message)
 
 if __name__ == "__main__":
     main()
