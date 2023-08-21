99f62c1fc0a0a50c5deefb35984e48a06aa84c99
AbhayGRT <abhaymaurya9769@gmail.com>
2023-08-21 15:08:21 +0530

    Second Commit

---
+++ ssl_expiry_check.py
@@ -0,0 +1,36 @@
+import ssl
+import socket
+import datetime
+import requests
+import os
+
+def get_ssl_expiry(domain):
+    context = ssl.create_default_context()
+    with socket.create_connection((domain, 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=domain) as sslsock:
+            cert = sslsock.getpeercert()
+            return datetime.datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
+
+def main():
+    domains = ["example1.com", "example2.com", "example3.com"]  # Add your domains here
+    for domain in domains:
+        expiry_date = get_ssl_expiry(domain)
+        remaining_days = (expiry_date - datetime.datetime.now()).days
+        message = (
+            f"SSL Expiry Alert\n"
+            f"* Domain : {domain}\n"
+            f"* Warning : The SSL certificate for {domain} will expire in {remaining_days} days."
+        )
+        payload = {"text": message}
+        slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+        if slack_webhook_url:
+            response = requests.post(slack_webhook_url, json=payload)
+            if response.status_code == 200:
+                print(f"Alert sent for {domain}")
+            else:
+                print(f"Failed to send alert for {domain}: {response.status_code}")
+        else:
+            print("No Slack webhook URL provided. Skipping alert.")
+
+if __name__ == "__main__":
+    main()
